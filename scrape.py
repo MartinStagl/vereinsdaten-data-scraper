3dddd5a0182eae4f7ddb2cfab92c38195d380d5a
mstagl <mar.stagl@gmai.com>
2022-08-30 23:23:15 +0200

    Updated Database connection

--- scrape.py
+++ scrape.py
@@ -39,8 +39,9 @@ def to_database(data):
         #mp.substitution_player = player["substitution_player"]
         #mp.substitution_minute = player["substitution_minute"]
         #home_team.players.append(p)
-        dbc.insert_player(p)
         home_team.players.append(mp)
+        dbc.insert_player(p)
+
 
         #home_team.players.append(player)
 
@@ -59,9 +60,10 @@ def to_database(data):
         #mp.starting_minute = player["starting_minute"]
         #mp.substitution_player = player["substitution_player"]
         #mp.substitution_minute = player["substitution_minute"]
+        away_team.players.append(mp)
         dbc.insert_player(p)
         #m.players.append(mp)
-        away_team.players.append(mp)
+
 
     m.away_team = away_team
     m.home_team = home_team
