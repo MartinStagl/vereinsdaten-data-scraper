c6ff9f4c4ced4444d926e1f4c3276ca891198e6c
mstagl <mar.stagl@gmai.com>
2022-08-30 22:38:11 +0200

    Updated Database connection

--- dao/setup.py
+++ dao/setup.py
@@ -54,10 +54,10 @@ class Match(Base):
     result= Column(Integer,nullable=True)
 
     away_team_id=Column(ForeignKey('teams.id'))
-    away_team = relationship("Team", back_populates="teams", foreign_keys="[Match.away_team_id]")
+    away_team = relationship("Team",  foreign_keys="[Match.away_team_id]")
 
     home_team_id = Column(ForeignKey('teams.id'))
-    home_team = relationship("Team", back_populates="teams", foreign_keys="[Match.home_team_id]")
+    home_team = relationship("Team",  foreign_keys="[Match.home_team_id]")
 
     #players = relationship('MatchPlayer', back_populates='match')
 
